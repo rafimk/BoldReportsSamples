775cb1abd1dd606014f2540bdc29e68dc269dc1f
rafimk <rafi.genius.cs@gmail.com>
2023-12-12 08:14:50 +0400

    added license

--- BoldReportsSamples.Api/Controllers/WeatherForecastController.cs
+++ BoldReportsSamples.Api/Controllers/WeatherForecastController.cs
@@ -30,56 +30,58 @@ namespace BoldReportsSamples.Api.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost]
+        public ActionResult Export(string writerFormat)
+        {
+            // Here, we have loaded the sales-order-detail sample report from application the folder wwwroot\Resources.
+            FileStream inputStream = new FileStream("sales-order-detail.rdl", FileMode.Open, FileAccess.Read);
+            MemoryStream reportStream = new MemoryStream();
+            inputStream.CopyTo(reportStream);
+            reportStream.Position = 0;
+            inputStream.Close();
+            BoldReports.Writer.ReportWriter writer = new BoldReports.Writer.ReportWriter();
+
+            string fileName = null;
+            WriterFormat format;
+            string type = null;
+
+            if (writerFormat == "PDF")
+            {
+                fileName = "sales-order-detail.pdf";
+                type = "pdf";
+                format = WriterFormat.PDF;
+            }
+            else if (writerFormat == "Word")
+            {
+                fileName = "sales-order-detail.docx";
+                type = "docx";
+                format = WriterFormat.Word;
+            }
+            else if (writerFormat == "CSV")
+            {
+                fileName = "sales-order-detail.csv";
+                type = "csv";
+                format = WriterFormat.CSV;
+            }
+            else
+            {
+                fileName = "sales-order-detail.xlsx";
+                type = "xlsx";
+                format = WriterFormat.Excel;
+            }
+
+            writer.LoadReport(reportStream);
+            MemoryStream memoryStream = new MemoryStream();
+            writer.Save(memoryStream, format);
+
+            // Download the generated export document to the client side.
+            memoryStream.Position = 0;
+            FileStreamResult fileStreamResult = new FileStreamResult(memoryStream, "application/" + type);
+            fileStreamResult.FileDownloadName = fileName;
+            return fileStreamResult;
+        }
     }
 
-    [HttpPost]
-    public IActionResult Export(string writerFormat)
-    {
-        // Here, we have loaded the sales-order-detail sample report from application the folder wwwroot\Resources.
-        FileStream inputStream = new FileStream("sales-order-detail.rdl", FileMode.Open, FileAccess.Read);
-        MemoryStream reportStream = new MemoryStream();
-        inputStream.CopyTo(reportStream);
-        reportStream.Position = 0;
-        inputStream.Close();
-        BoldReports.Writer.ReportWriter writer = new BoldReports.Writer.ReportWriter();
-
-        string fileName = null;
-        WriterFormat format;
-        string type = null;
-
-        if (writerFormat == "PDF")
-        {
-            fileName = "sales-order-detail.pdf";
-            type = "pdf";
-            format = WriterFormat.PDF;
-        }
-        else if (writerFormat == "Word")
-        {
-            fileName = "sales-order-detail.docx";
-            type = "docx";
-            format = WriterFormat.Word;
-        }
-        else if (writerFormat == "CSV")
-        {
-            fileName = "sales-order-detail.csv";
-            type = "csv";
-            format = WriterFormat.CSV;
-        }
-        else
-        {
-            fileName = "sales-order-detail.xlsx";
-            type = "xlsx";
-            format = WriterFormat.Excel;
-        }
-
-        writer.LoadReport(reportStream);
-        MemoryStream memoryStream = new MemoryStream();
-        writer.Save(memoryStream, format);
-
-        // Download the generated export document to the client side.
-        memoryStream.Position = 0;
-        FileStreamResult fileStreamResult = new FileStreamResult(memoryStream, "application/" + type);
-        fileStreamResult.FileDownloadName = fileName;
-        return fileStreamResult;
-    }
+    
 }
--- BoldReportsSamples.Api/Program.cs
+++ BoldReportsSamples.Api/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+Bold.Licensing.BoldLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
